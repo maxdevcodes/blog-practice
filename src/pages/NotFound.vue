53d7ebf3cd79e3c98e4416d2718ef7106ab04a62
Javi García <maxdevcodes@gmail.com>
2020-09-23 18:51:54 +0200

    back button for 404 page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,15 +1,29 @@
 <template>
-    <div>
+    <div class="container">
         <h1>404, page not found</h1>
+        <button class="btn" @click="$router.go(-1)">Go back</button>
     </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
 <style lang="scss" scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
 
+.btn {
+    background: #41b883;
+    border-radius: 5px;
+    border: none;
+    color: white;
+    cursor: pointer;
+    padding: 10px 15px;
+    margin: 10px auto;
+}
 </style>
