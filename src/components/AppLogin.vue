c580ff47db66bd91fe15a537e056e8a6f0658ca0
Javi García <maxdevcodes@gmail.com>
2020-09-28 18:49:50 +0200

    refactor login component

--- src/components/AppLogin.vue
+++ src/components/AppLogin.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
-        <form action="#">
-            <input type="text" name="user" id="user">
-            <input type="password" name="password" id="password">
+        <form action="#" @submit="authenticate">
+            <label for="user">Username:</label>
+            <input class="fullwidth-input" type="text" name="user" id="user">
+            <label for="password">Password:</label>
+            <input class="fullwidth-input" type="password" name="password" id="password">
             <button class="btn" type="submit">Login</button>
         </form>
-        <button class="btn" @click="authenticate">Test auth</button>
     </div>
 </template>
 
@@ -21,6 +22,24 @@
 </script>
 
 <style lang="scss" scoped>
+form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    max-width: 300px;
+    margin: 0 auto;
+
+    .fullwidth-input {
+        width: 100%;
+        margin-bottom: 15px;
+    }
+    input {
+        width: auto;
+        margin: 10px auto;
+    }
+}
+
 .btn {
     background: #41b883;
     border-radius: 5px;
